f320e1696c9bf0aa8c35eac618fc6714bb7336ac
Quoc Thai Nguyen Truong <thai.nguyen@berkeley.edu>
2017-10-08 10:52:37 -0700

    Add update and delete tweet, get Date to type date

--- Server/src/graphql/resolvers/index.js
+++ Server/src/graphql/resolvers/index.js
@@ -1,7 +1,17 @@
+import GraphQLDate from 'graphql-date';
+
 import TweetResolvers from './tweet-resolvers';
 
 export default {
+  Date: GraphQLDate,
   Query: {
+    getTweet: TweetResolvers.getTweet,
     getTweets: TweetResolvers.getTweets
+  },
+
+  Mutation: {
+    createTweet: TweetResolvers.createTweet,
+    updateTweet: TweetResolvers.updateTweet,
+    deleteTweet: TweetResolvers.deleteTweet
   }
 };
--- Server/src/graphql/resolvers/tweet-resolvers.js
+++ Server/src/graphql/resolvers/tweet-resolvers.js
@@ -2,5 +2,21 @@ import Tweet from '../../models/Tweet';
 
 // Give me all the tweets
 export default {
-  getTweets: () => Tweet.find({})
+  getTweet: (_, { _id }) => Tweet.findById(_id),
+  getTweets: () => Tweet.find({}).sort({ createdAt: -1 }),
+  createTweet: (_, args) => Tweet.create(args),
+  updateTweet: (_, { _id, ...rest }) =>
+    Tweet.findByIdAndUpdate(_id, rest, { new: true }),
+  deleteTweet: async (_, { _id }) => {
+    try {
+      await Tweet.findByIdAndRemove(_id);
+      return {
+        message: 'Delete Success!'
+      };
+    } catch (e) {
+      throw e;
+    }
+  }
 };
+
+// {new: true} here mean return the new object, not the old
