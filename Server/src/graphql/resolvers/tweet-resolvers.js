7583baecc3cc6fa698ab2a3028818678dfbc9d36
Quoc Thai Nguyen Truong <thai.nguyen@berkeley.edu>
2017-10-08 14:25:22 -0700

    Authentication for user and tweet api

--- Server/src/graphql/resolvers/tweet-resolvers.js
+++ Server/src/graphql/resolvers/tweet-resolvers.js
@@ -3,22 +3,47 @@ import { requireAuth } from '../../services/auth';
 
 // Give me all the tweets
 export default {
-  getTweet: (_, { _id }) => Tweet.findById(_id),
-  getTweets: () => Tweet.find({}).sort({ createdAt: -1 }),
+  getTweet: async (_, { _id }, { user }) => {
+    try {
+      await requireAuth(user);
+      return Tweet.findById(_id);
+    } catch (e) {
+      throw e;
+    }
+  },
+  getTweets: async (_, args, { user }) => {
+    try {
+      await requireAuth(user);
+      return Tweet.find({}).sort({ createdAt: -1 });
+    } catch (e) {
+      throw e;
+    }
+  },
   // createTweet: async (_, args, ctx) => {
   createTweet: async (_, args, { user }) => {
     // console.log('====================');
     // console.log('Context', ctx);
     // console.log('====================');
-
-    await requireAuth(user);
-
-    return Tweet.create(args);
-  },
-  updateTweet: (_, { _id, ...rest }) =>
-    Tweet.findByIdAndUpdate(_id, rest, { new: true }),
-  deleteTweet: async (_, { _id }) => {
     try {
+      await requireAuth(user);
+      return Tweet.create(args);
+    } catch (e) {
+      throw e;
+    }
+  },
+  updateTweet: async (_, { _id, ...rest }, { user }) => {
+    try {
+      await requireAuth(user);
+
+      // {new: true} here mean return the new object, not the old
+      return Tweet.findByIdAndUpdate(_id, rest, { new: true });
+    } catch (e) {
+      throw e;
+    }
+  },
+  deleteTweet: async (_, { _id }, { user }) => {
+    try {
+      await requireAuth(user);
       await Tweet.findByIdAndRemove(_id);
       return {
         message: 'Delete Success!'
@@ -28,5 +53,3 @@ export default {
     }
   }
 };
-
-// {new: true} here mean return the new object, not the old
--- Server/src/graphql/resolvers/user-resolvers.js
+++ Server/src/graphql/resolvers/user-resolvers.js
@@ -1,16 +1,23 @@
 import User from '../../models/User';
+import { requireAuth } from '../../services/auth';
 
 export default {
   // signup: (_, { fullName, username, password, email, avatar }) => {
   signup: async (_, { fullName, ...rest }) => {
     // "Thai Nguyen Truong" ==> firstName = "Thai", lastname = "Nguyen Truong"
-    const [firstName, ...lastName] = fullName.split(' ');
 
-    const user = await User.create({ firstName, lastName, ...rest });
+    try {
+      const [firstName, ...lastName] = fullName.split(' ');
 
-    return {
-      token: user.createToken()
-    };
+      const user = await User.create({ firstName, lastName, ...rest });
+
+      return {
+        token: user.createToken()
+      };
+    } catch (e) {
+      throw e;
+    }
+
     // return User.create({
     //   firstName,
     //   lastName,
@@ -23,16 +30,32 @@ export default {
   },
 
   login: async (_, { email, password }) => {
-    const user = await User.findOne({ email });
+    try {
+      const user = await User.findOne({ email });
 
-    if (!user) {
-      throw new Error('User not exist!');
+      if (!user) {
+        throw new Error('User not exist!');
+      }
+
+      if (!user.authenticateUser(password)) {
+        throw new Error('Password not match!!');
+      }
+
+      return {
+        token: user.createtoken()
+      };
+    } catch (e) {
+      throw e;
     }
+  },
 
-    if (!user.authenticateUser(password)) {
-      throw new Error('Password not match!!');
+  me: async (_, args, { user }) => {
+    try {
+      const me = await requireAuth(user);
+
+      return me;
+    } catch (e) {
+      throw e;
     }
-
-    return user;
   }
 };
